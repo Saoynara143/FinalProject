40aac4f3da90ce140b1c7542abf78cfcd042303a
Артем <ckripa2013@gmail.com>
2023-03-15 14:53:05 +0300

    Создание кода

---
+++ FinalTask/Program.cs
@@ -0,0 +1,29 @@
+﻿
+string[] array1;
+Console.WriteLine("Введите массив строк, через запятую, без пробелов:");
+string stroka = Console.ReadLine();
+array1 = stroka.Split(',');
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+Console.WriteLine("Массив состоящий из строк, чья длинна меньше, либо ровна 3 символа:");
+PrintArray(array2);
